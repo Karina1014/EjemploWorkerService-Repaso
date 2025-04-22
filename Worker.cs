8dba43a41bc5a4cc4df21f6798608e53df5c4a7e
Karina1014 <janneth_wi15@hotmail.com>
2025-04-22 07:02:25 -0500

    Agregar archivos de proyecto.

---
+++ Worker.cs
@@ -0,0 +1,28 @@
+namespace ASPWorker;
+
+public class Worker : BackgroundService
+{
+    private readonly ILogger<Worker> _logger;
+    private string _path = Directory.GetCurrentDirectory() + @"\Files\";
+    private IFileData _fileData;
+    private int _count = 0;
+
+    public Worker(ILogger<Worker> logger, IFileData fileData)
+    {
+        _logger = logger;
+        _fileData = fileData;
+    }
+
+    protected override async Task ExecuteAsync(CancellationToken stoppingToken)
+    {
+        while (!stoppingToken.IsCancellationRequested)
+        {
+            if (_logger.IsEnabled(LogLevel.Information))
+            {
+                _logger.LogInformation("Worker running at: {time}", DateTimeOffset.Now);
+                await _fileData.Create($"{_path}{_count++}.txt");
+            }
+            await Task.Delay(1000, stoppingToken);
+        }
+    }
+}
